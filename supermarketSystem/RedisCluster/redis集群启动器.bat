03d702f1d21ce2ff7b6a1a0cd20bc5d95045c1e5
xianing-lll <58456001+xianing-lll@users.noreply.github.com>
2020-01-20 13:23:39 +0800

    带集群

---
+++ supermarketSystem/RedisCluster/redis集群启动器.bat
@@ -0,0 +1,6 @@
+start /d "D:\RedisCluster\8001" start.bat
+start /d "D:\RedisCluster\8002" start.bat
+start /d "D:\RedisCluster\8003" start.bat
+start /d "D:\RedisCluster\8004" start.bat
+start /d "D:\RedisCluster\8005" start.bat
+start /d "D:\RedisCluster\8006" start.bat
